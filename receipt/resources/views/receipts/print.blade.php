a7e071b5fc6c64184a5f9e9222cbf6a7089b1cd0
CarletMarzelYna <126882623+CarletMarzelYna@users.noreply.github.com>
2026-03-10 13:31:45 +0800

    print layout ok

--- receipt/resources/views/receipts/print.blade.php
+++ receipt/resources/views/receipts/print.blade.php
@@ -3,34 +3,53 @@
 <head>
     <meta charset="utf-8">
     <style>
+        @page {
+            margin-top: 0;
+            margin-left: 20px;
+            margin-right: 0px;
+        }
+
         body{
             font-family: "Lucida Console", "Courier New", monospace;
             font-size: 12px;
             margin: 0;
+            padding: 0;
         }
+
         .container{
             width:100%;
         }
+
         table{
-            width:45%;
+            width:44%;
             border-collapse: collapse;
         }
+
         td{
             padding:5px;
+        }
+
+        @media print {
+            body{
+                margin-top: 0;
+                margin-left: 20px;
+                margin-right: 0px;
+                padding:0;
+            }
         }
     </style>
 </head>
 
 <body>
-    <table border="1" size="">
+    <table>
         <tr>
-            <td colspan="2" style="padding-top: 50px;">{{ $receipt->receipt_date }}</td>
+            <td colspan="2" style="padding-top: 175px;">{{ $receipt->receipt_date }}</td>
             <td colspan="2"></td>
         </tr>
 
         <tr>
-            <td colspan="3">PROVINCIAL TREASURY OFFICE</td>
-            <td>100</td>
+            <td colspan="3" style="padding-top: 15px;">PROVINCIAL TREASURY OFFICE</td>
+            <td style="padding-top: 15px; text-align: right;">100</td>
         </tr>
         <tr>
             <td colspan="4">{{ $receipt->payer_name }}</td>
@@ -39,35 +58,42 @@
         <tr>
             <td colspan="2"></td>
             <td></td>
-            <td style="padding-top: 50px;">P {{ number_format($receipt->amount,2) }}</td>
+            <td style="padding-top: 43px; text-align: right;">{{ number_format($receipt->amount,2) }}</td>
         </tr>
         <tr>
-            <td colspan="4">AMOUNT IN WORDS</td>
-        </tr>
-
-        <tr>
-            <td>CASH</td>
-            <td>DRAWEE BANK</td>
-            <td>NUMBER</td>
-            <td>DATE</td>
-        </tr>
-        <tr>
-            <td>CHECK</td>
-            <td></td>
+            <td colspan="2" style="padding-bottom: 190px;"></td>
             <td></td>
             <td></td>
         </tr>
         <tr>
-            <td>MONEY ORDER</td>
+            <td colspan="2"></td>
             <td></td>
-            <td></td>
-            <td></td>
+            <td style="padding-top: 10px; text-align: right;">{{ number_format($receipt->amount,2) }}</td>
+        </tr>
+        <tr>
+            <td colspan="4" style="padding-top: 28px;">AMOUNT IN WORDS</td>
         </tr>
 
         <tr>
+            <td colspan="4" style="padding-top: 20px;">X</td>
+        </tr>
+        <tr>
+            <td>X</td>
             <td></td>
             <td></td>
-            <td colspan="2" style="padding-top: 50px;">ALMA CRUZ</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>X</td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td></td>
+            <td colspan="2" style="padding-top: 40px; font-size: 20px">ALMA CRUZ</td>
         </tr>
 
     </table>
