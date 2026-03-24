f2c438ca5aaea0d2e010719ee2fc7f3c12b50274
CarletMarzelYna <126882623+CarletMarzelYna@users.noreply.github.com>
2026-03-24 11:34:39 +0800

    settlement & liquidation (normal size) others (15px)

--- receipt/resources/views/receipts/print.blade.php
+++ receipt/resources/views/receipts/print.blade.php
@@ -77,7 +77,7 @@
             }
         @endphp
         <tr>
-            <td style="font-size: 15px; height:227px; padding-top:18px; overflow:hidden; vertical-align: top; white-space: pre-line; max-width: 205px;">
+            <td style="height:227px; padding-top:18px; overflow:hidden; vertical-align: top; white-space: pre-line; max-width: 205px;">
                 @if(count($natureLines))
                     {{ implode("\n", $natureLines) }}
                 @else 
